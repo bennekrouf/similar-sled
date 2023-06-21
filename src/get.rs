de6bdc967e8f91cc1775b83b5c8d41fa6bcd4a9b
Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-06-21 06:16:35 +0200

    init

--- src/get.rs
+++ src/get.rs
@@ -3,6 +3,45 @@ use crate::utils;
 use rocket_contrib::json::Json;
 use crate::models::Database;
 
+#[get("/similar/<chapter_no>")]
+pub fn get_similar(chapter_no: u8, dbs: State<Database>) -> Json<Vec<(String, Vec<(String, String, u32)>)>> {
+   // Retrieve the list of similars from the similar_db
+    let similars: Vec<(String, Vec<(String, String, u32)>)> = dbs.similar_db
+        .iter()
+        .map(|result| {
+            let (key, value) = result.expect("Failed to retrieve similar");
+            let key_string = String::from_utf8_lossy(&key).into_owned();
+            let references: Vec<(u32, u32)> =
+                bincode::deserialize(&value).expect("Failed to deserialize references");
+
+            let updated_references: Vec<(String, String, u32)> = references
+                .iter()
+                // .filter(|&&reference| reference.0 == u32::from(chapter_no)) // Filter by chapter_no
+                .map(|&reference| {
+                    let ayat = reference.1;
+                    println!("{} {}", reference.0, reference.1);
+                    let chapter_name = utils::get_chapter_name(&dbs.chapter_db, reference.0 as u8)
+                     .unwrap_or_else(|_| Some(String::from("Default Chapter")))
+                    .unwrap();
+
+                    let verse_text = match utils::get_verse_by_chapter_and_ayat(&dbs.verse_db, reference.0, ayat) {
+                        Ok(Some(text)) => text,
+                        Ok(None) => String::from("Verse not found"),
+                        Err(_) => String::from("Error retrieving verse"),
+                    };
+
+                    (verse_text, chapter_name, ayat)
+            })
+            .collect::<Vec<(String, String, u32)>>();
+
+            (key_string, updated_references)
+        })
+        .collect();
+
+    // Return the similars as JSON
+    Json(similars)
+}
+
 
 #[get("/verse/<chapter_no>")]
 pub fn get_verse(chapter_no: u8, dbs: State<Database>) -> Json<serde_json::Value> {
@@ -19,8 +58,8 @@ pub fn get_verse(chapter_no: u8, dbs: State<Database>) -> Json<serde_json::Value
     Json(json_value)
 }
 
-#[get("/similar/<chapter_no>")]
-pub fn get_similar(chapter_no: u8, dbs: State<Database>) -> Json<Vec<(String, Vec<(String, String, u32)>)>> {
+#[get("/similars")]
+pub fn get_similars(dbs: State<Database>) -> Json<Vec<(String, Vec<(String, String, u32)>)>> {
    // Retrieve the list of similars from the similar_db
     let similars: Vec<(String, Vec<(String, String, u32)>)> = dbs.similar_db
         .iter()
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 // use sled::Db;
+use std::path::Path;
 mod utils;
 mod models;
 mod init_db;
-
+use std::env;
 use rocket::{routes, Rocket};
 use rocket::fairing::AdHoc;
 // use rocket_contrib::json::Json;
@@ -13,9 +14,38 @@ mod get;
 fn rocket() -> Rocket {
     rocket::ignite()
     .attach(AdHoc::on_attach("Verse Database1", |rocket| {
-        let chapter_db = sled::open("db/chapter_db").expect("Failed to open chapter database");
-        let verse_db = sled::open("db/verse_db").expect("Failed to open verse database");
-        let similar_db = sled::open("db/similar_db").expect("Failed to open similar database");
+       
+         // Retrieve the command-line arguments
+        let args: Vec<String> = env::args().collect();
+
+        // Expect the second command-line argument to be the path to the "data" folder
+        if args.len() < 2 {
+            println!("Usage: ./my_program <data_folder_path>");
+             std::process::exit(1);
+        }
+
+        let data_folder_path = &args[1];
+
+        // Get the current directory of the executable
+        let exe_dir = env::current_exe()
+            .expect("Failed to get executable directory")
+            .parent()
+            .expect("Failed to get parent directory")
+            .to_path_buf();
+
+        // Construct the absolute path to the data folder using the provided path
+        let data_folder = Path::new(data_folder_path);
+        let absolute_data_folder_path = exe_dir.join(data_folder);
+
+        // Construct the absolute paths to the database files
+        let chapter_db_path = absolute_data_folder_path.join("chapter_db");
+        let verse_db_path = absolute_data_folder_path.join("verse_db");
+        let similar_db_path = absolute_data_folder_path.join("similar_db");
+
+        // Open the Sled databases using the adjusted file paths
+        let chapter_db = sled::open(chapter_db_path).expect("Failed to open chapter database");
+        let verse_db = sled::open(verse_db_path).expect("Failed to open verse database");
+        let similar_db = sled::open(similar_db_path).expect("Failed to open similar database");
 
         let database = models::Database {
             chapter_db,
@@ -29,31 +59,9 @@ fn rocket() -> Rocket {
         let new_rocket = rocket.manage(database);
         Ok(new_rocket)
     }))
-    .mount("/", routes![get::get_verse, get::get_similar])
+    .mount("/", routes![get::get_verse, get::get_similar, get::get_similars])
 }
 
-
-
 fn main() {
-    // let chapter_db: sled::Db = sled::open("db/chapter_db").unwrap();
-    // let verse_db: sled::Db = sled::open("db/verse_db").unwrap();
-    // let similar_db: sled::Db = sled::open("db/similar_db").unwrap();
-    // for i in 1..=114 {
-    //     let chapter_name = utils::get_chapter_name(&db, i).unwrap();
-    //     println!("{:?}", chapter_name.unwrap());
-    // }
-    
-    // let dbs = vec![&chapter_db, &verse_db, &similar_db];
-    // let dbs = vec![&similar_db];
-
-    // for db in dbs {
-    //     utils::count(&db);
-    //     let size = db.size_on_disk().expect("Failed to compute size");
-    //     println!("Size of sled database: {} bytes", size);
-    // }
-
-    // println!("{:?}", utils::get_chapter_name(&chapter_db, 2).unwrap());
-    // println!("{:?}", utils::get_verses_by_chapter(&verse_db, 2).unwrap());
-
     rocket().launch();
 }
