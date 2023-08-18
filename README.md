8f5e8071e5f9c0c53956588cb3e8d7916f0a634a
Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-08-18 16:46:13 +0200

    Work on text

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+# Run
 
 ````bash
-cargo watch -c -w src -x run
+APP_ENV=local cargo watch -c -w src -x run
+APP_ENV=staging cargo watch -c -w src -x run
+APP_ENV=production cargo watch -c -w src -x run
+```
+
+```
+APP_ENV=local cargo run
+APP_ENV=staging cargo run
+APP_ENV=production cargo run
 ```
--- src/utils/yml_path.rs
+++ src/utils/yml_path.rs
@@ -3,18 +3,26 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
 use serde_yaml;
+use std::env;
 
 #[derive(Deserialize)]
 struct Config {
     macos_path: String,
     debian_path: String,
 }
-
 pub fn get_data_folder_path() -> PathBuf {
     let mut path = PathBuf::new();
 
+    // Determine which config file to load
+    let env = env::var("APP_ENV").unwrap_or("local".to_string()); // Default to local if not set
+    let config_filename = match env.as_str() {
+        "staging" => "config.staging.yml",
+        "production" => "config.production.yml",
+        _ => "config.local.yml", // Default to local
+    };
+
     // Read the config file
-    let mut config_file = File::open("config.yml").expect("Failed to open config file");
+    let mut config_file = File::open(config_filename).expect("Failed to open config file");
     let mut config_content = String::new();
     config_file.read_to_string(&mut config_content).expect("Failed to read config file");
 
