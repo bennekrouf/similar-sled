88a5a077083c6d7a805a79df41ccf7264a61c884
Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-08-19 16:57:12 +0200

    Fix on build script

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
 
 ENV=$1
 
-APP_ENV=$ENV cargo build --release
-mv target/release/similars-sled /home/similar/$ENV/similars-sled-$ENV
-cp -r config.yml /home/similar/$ENV/
+APP_ENV=${ENV} cargo build --release
+mv target/release/similars-sled /home/similar/${ENV}/similars-sled-${ENV}
+cp -r config.${ENV}.yml /home/similar/${ENV}/
