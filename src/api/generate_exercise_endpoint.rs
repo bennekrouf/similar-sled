f64d0a40316db3e1fa8cedcdb630d98c2a491d46
Bennekrouf <mohamed.bennekrouf@gmail.com>
2024-01-19 20:36:29 +0100

    Fix exercise API

--- src/api/generate_exercise_endpoint.rs
+++ src/api/generate_exercise_endpoint.rs
@@ -1,13 +1,15 @@
 use rocket::{get, State};
 use rocket::serde::json::Json;
 
-use crate::xhr_guard::XHR;
 use crate::models::{Database, Exercise};
 use crate::domain::exercise::get_exercises::get_exercises;
 use crate::utils::parse_ranges::parse_ranges;
 
 #[get("/exercise_list?<ranges>")]
-pub fn generate_exercise_list_endpoint(ranges: Option<String>, dbs: &State<Database>, _xhr: XHR) -> Json<Vec<Exercise>> {
+pub fn generate_exercise_list_endpoint(
+    dbs: &State<Database>,
+    ranges: Option<String> ,
+) -> Json<Vec<Exercise>> {
     let parsed_ranges = ranges.as_ref().map(|r| parse_ranges(r));
     let exercises = get_exercises(&dbs, &parsed_ranges);
     Json(exercises)
